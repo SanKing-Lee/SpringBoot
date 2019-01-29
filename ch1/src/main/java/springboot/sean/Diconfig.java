1decc488202055eb6a8b7f19694237a4f67c211b
SanKing_Lee <1310179113@qq.com>
2019-01-29 13:58:30 +0800

    reconsturct the project

--- ch1/src/main/java/springboot/sean/Diconfig.java
+++
@@ -1,9 +0,0 @@
-package springboot.sean;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("springboot.sean")
-public class Diconfig {
-}
