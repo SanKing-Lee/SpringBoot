77ff958f0e7c469fb88314c09f5adf6d7b5bb45c
Sean <1310179113@qq.com>
2019-02-12 14:10:02 +0800

    test

--- src/main/java/ch3/taskexecutor/AsyncTaskService.java
+++ src/main/java/ch3/taskexecutor/AsyncTaskService.java
@@ -12,6 +12,6 @@ public class AsyncTaskService {
 
     @Async
     public void executeAsyncTaskPlus(Integer i){
-        System.out.println("执行异步任务plus：" + (i+1));
+        System.out.println("执行异步任务+1：" + (i+1));
     }
 }
